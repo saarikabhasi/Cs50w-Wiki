f4e71bc14a6099564d89202d199c5eb331d791db
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2020-07-12 20:17:03 -0400

    pre-code

--- encyclopedia/templatetags/markdown_edit.py
+++ encyclopedia/templatetags/markdown_edit.py
@@ -20,6 +20,9 @@ class markdown(object):
         self.previouslinespace = 0
         self.count =0
         self.markdown_string = value
+        self.code_area = False
+
+        # Regex pattern 
         self.patterns = {
             "heading" :"#+\s", #heading
             "ul": r"^\s*(\-\s|\*\s)([\w+(\[|\^&+\-.',%\(\/)=!\:>\'\"\*\s]+?)(?<=$)", #unordered list
@@ -31,18 +34,21 @@ class markdown(object):
             "bold_and_italic":r"\*\*\*(.+?)\*\*\*",
             "strikethrough" :r"(~~)(?=\S)(.+?[*_]*)(?<=\S)\1", #strikethrough
             "code":r"```((.+?\n)+)", #blocked code
+            #code single line:(?:`{3})+(?<=\S)(\w*)(?:`{3})+
 
             
         }
-        # self.substitute_patterns ={
-        #     "li_opentag":"\n<li>\n",
-        #     "li_closetag":"\n</li>\n",
-        #     "ul_li_tag":"\n<ul>\n<li>\2</li>\n",
-        #     "hr_tag" : "\n<hr>\n",
-        #     "bold":"<strong>\2</strong>",
-        #     "italic":"<em>\2</em>",
-        #     "strikethrough":"<s>\2</s>",
-        # }
+        # HTML tags 
+        self.substitute_patterns ={
+            "li_tag":r"\n<li>\2</li>\n", #li tag
+            "ul_li_tag":r"\n<ul>\n<li>\2</li>\n", # new un ordered list tag
+            "hr_tag" : r"\n<hr>\n", #line break tag 
+            "bold":r"<strong>\2</strong>", #bold
+            "italic":r"<em>\2</em>", #italic
+            "strikethrough":r"<s>\2</s>", #strikethrough
+            "bold_and_italic":r"<strong><em>\2</em></strong>", #Bold and italic
+            "code":r"<pre><code>\1</code></pre>",
+        }
 
     # close all the opened tags of unordered list
     def close_list(self,line):
@@ -74,11 +80,11 @@ class markdown(object):
         if self.currentlinespace-self.previouslinespace == 0: 
 
             if self.ultag_is_open == True:                       
-                line = unordered_list.sub(r"\n<li>\2</li>\n",line)
+                line = unordered_list.sub(self.substitute_patterns["li_tag"],line)
 
             else:
                 #first li in the list
-                line = unordered_list.sub(r"\n<ul>\n<li>\2</li>\n",line)
+                line = unordered_list.sub(self.substitute_patterns["ul_li_tag"],line)
                 self.ultag_is_open = True #set ul tag to open
                 self.previouslinespace = self.currentlinespace
 
@@ -89,16 +95,16 @@ class markdown(object):
 
                 if self.sub_ultag_is_open :
                     if self.count >= 1:
-                        line = unordered_list.sub(r"\n<ul>\n<li>\2</li>\n",line) 
-                    line = unordered_list.sub(r"\n<li>\2</li>\n",line) 
+                        line = unordered_list.sub(self.substitute_patterns["ul_li_tag"],line) 
+                    line = unordered_list.sub(self.substitute_patterns["li_tag"],line) 
                 else:
                    
                     self.sub_ultag_is_open = True
                     self.count +=1
-                    line = unordered_list.sub(r"\n<ul>\n<li>\2</li>\n",line)
+                    line = unordered_list.sub(self.substitute_patterns["ul_li_tag"],line)
             else:
 
-                line = unordered_list.sub(r"\n<ul>\n<li>\2</li>\n",line)
+                line = unordered_list.sub(self.substitute_patterns["ul_li_tag"],line)
                 self.ultag_is_open = True 
             self.previouslinespace = self.currentlinespace
 
@@ -108,7 +114,7 @@ class markdown(object):
             #close all previous nested list
             line =self.close_list(line)
 
-            line = unordered_list.sub(r"<ul>\n<li>\2</li>",line)
+            line = unordered_list.sub(self.substitute_patterns["ul_li_tag"],line)
             self.ultag_is_open = True #set ul tag to open
             self.previouslinespace = self.currentlinespace
 
@@ -127,106 +133,132 @@ class markdown(object):
     
     #HR tag
     def hr(self,hr,line):
-        line = hr.sub("\n<hr>\n",line)
+        line = hr.sub(self.substitute_patterns["hr_tag"] ,line)
         return line
     
     #Bold
     def bold(self,bold,line):
-        line = bold.sub(r"\n<strong>\2</strong>\n",line)
+        line = bold.sub(self.substitute_patterns["bold"],line)
         return line
 
     #Italic
     def italic(self,italic,line):
-        line = italic.sub(r"\n<em>\2</em>\n",line)
+        line = italic.sub(self.substitute_patterns["italic"],line)
         return line
 
     #Strikethrough
     def strikethrough(self,strikethrough,line):
-        line = strikethrough.sub(r"\n<s>\2</s>\n",line)
+        line = strikethrough.sub(self.substitute_patterns["strikethrough"],line)
         return line
 
     #Bold and italic
     def bold_and_italic(self,bold_and_italic,line):
-        line = bold_and_italic.sub(r"\n<strong><em>\2</em></strong>\n")
+        line = bold_and_italic.sub(self.substitute_patterns["bold_and_italic"],line )
         return line
     #code
     def code(self,code,line):
-        line = code.sub(r"\n<pre><code>\1</code></pre>\n",line)
+        line = code.sub(self.substitute_patterns["code"],line)
         print("After change pre-code",line)
         return line
     
 
     # markdown to html main function
     def markdown_parser(self,markdown_string):
-        print("Coming inside of markdown_parser")
+        codeline =""
         for line in markdown_string.splitlines():
             print("LINE",line)
-            #heading    
-            heading = re.compile(self.patterns["heading"]) 
-            heading_matches = heading.search(line.strip())
+            print("self.code_area Before test",self.code_area )
 
-            if heading_matches != None:
-                
-                line=self.heading(heading_matches,line)
-
-            #unordered list
-            unordered_list = re.compile(self.patterns["ul"],re.MULTILINE)
-
-            if unordered_list.search(line):
-                line = self.list(line,unordered_list)
+            if self.code_area == True:
+                if "```" in line:
+                    self.code_area = False
+                    line = codeline+"</code></pre>"
+                    
+                    codeline=""
+                else:
+                    codeline +=line+'\n'
+                    print("line after change",codeline)
+                    continue
             else:
-                line = self.close_list(line) #close all opened list tags
-                self.previouslinespace = 0
+                if "```" in line:
+                    self.code_area = True
+                    codeline = "<pre><code>"
+            print("self.code_area After TEst",self.code_area )
+            if self.code_area ==False:
+                #heading    
+                heading = re.compile(self.patterns["heading"]) 
+                heading_matches = heading.search(line.strip())
 
-            #HR tag
-            hr =  re.compile(self.patterns["hr"])
-            if hr.search(line): 
-                line = self.hr(hr,line)
+                if heading_matches != None:
+                    
+                    line=self.heading(heading_matches,line)
 
-            #bold and italic
-            bold_and_italic = re.compile(self.patterns["bold_and_italic"])
-            if bold_and_italic.search(line):
-                line = self.bold_and_italic(bold_and_italic,line)
-                
-            #Bold tag
-            bold = re.compile(self.patterns["bold"])
-            if bold.search(line):
-                line = self.bold(bold,line)
+                #unordered list
+                unordered_list = re.compile(self.patterns["ul"],re.MULTILINE)
 
-            #italic tag
-            italic = re.compile(self.patterns["italic"])
-      
-            if italic.search(line):
-         
-                line = self.italic(italic,line)
-                
-            #Strikethrough
-            strikethrough = re.compile(self.patterns["strikethrough"])
+                if unordered_list.search(line):
+                    line = self.list(line,unordered_list)
+                else:
+                    line = self.close_list(line) #close all opened list tags
+                    self.previouslinespace = 0
+
+                #HR tag
+                hr =  re.compile(self.patterns["hr"])
+                if hr.search(line): 
+                    line = self.hr(hr,line)
+
+                #bold and italic
+                bold_and_italic = re.compile(self.patterns["bold_and_italic"])
+                if bold_and_italic.search(line):
+                    line = self.bold_and_italic(bold_and_italic,line)
+                    
+                #Bold tag
+                bold = re.compile(self.patterns["bold"])
+                if bold.search(line):
+                    line = self.bold(bold,line)
+
+                #italic tag
+                italic = re.compile(self.patterns["italic"])
         
-            if strikethrough.search(line):
-                line = self.strikethrough(strikethrough,line)
-
-
-            code = re.compile(self.patterns["code"])
-            print("Before change code",line)
-            if code.search(line):
-                line =self.code(code,line)
-            print("After change code",line)
+                if italic.search(line):
+         
+                    line = self.italic(italic,line)
+                
+                #Strikethrough
+                strikethrough = re.compile(self.patterns["strikethrough"])
             
-            if line.strip() != "": #check if line is not empty
-                if "<ul>" not in line and "</ul>" not in line:
-                    if "<li>" not in line and "</li>" not in line:
-                        if "<h" not in line and "<h" not in line:
-                            if "<code>" and "</code>" not in line:
-                                self.results+='\n'+"<p>"+line +"</p>"+'\n'
+                if strikethrough.search(line):
+                    line = self.strikethrough(strikethrough,line)
+
+                # code
+            
+
+                # code = re.compile(self.patterns["code"])
+                # print("Before change code",line)
+                # if code.search(line):
+                #     line =self.code(code,line)
+                #     print("After change code",line)
+            
+                if line.strip() != "": #check if line is not empty
+                    if "<ul>" not in line and "</ul>" not in line:
+                        if "<li>" not in line and "</li>" not in line:
+                            if "<h" not in line and "<h" not in line:
+                                if "<code>" not in line and "</code>" not in line:
+                                    if "<pre>" not in line and "</pre>" not in line:
+                                        self.results+='\n'+"<p>"+line +"</p>"+'\n'
+                                    else:
+                                        self.results += '\n'+ codeline +'\n'
+                                else:
+                                    self.results += '\n'+ codeline +'\n'
                             else:
                                 self.results += '\n'+ line +'\n'
                         else:
                             self.results += '\n'+ line +'\n'
                     else:
-                        self.results += '\n'+ line +'\n'
-                else:
-                    self.results += '\n'+ line+'\n'
+                        self.results += '\n'+ line+'\n'
+            else:
+                
+                self.results += '\n'+ codeline +'\n'
  
             print("RESULTS",self.results)  
         return self.results
