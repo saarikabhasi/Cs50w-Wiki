ca0584e690989fe95ea8ef0172b55037fc11133f
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2020-08-04 22:36:29 -0400

    initial commit of final working

--- encyclopedia/templatetags/markdown.py
+++ encyclopedia/templatetags/markdown.py
@@ -101,8 +101,8 @@ class markdown(object):
         print("OL LIST is being called")
         current_ol_line_space = len(line)-len(line.lstrip())
         #print("LINE",line)
-        #print("SPACE:current_ol_line_space",current_ol_line_space,"self.previous_ol_linespace")
-        #print("MINUS",current_ol_line_space-self.previous_ol_linespace )
+        print("SPACE:current_ol_line_space",current_ol_line_space,"self.previous_ol_linespace")
+        print("MINUS",current_ol_line_space-self.previous_ol_linespace )
         # If the line space is same as previous ordered list line space . 
         # Then the line is considered as same level the previous ol list.
 
@@ -316,9 +316,25 @@ class markdown(object):
         line_tobe_checked_for_heading = ""
         heading_check =False
         multi_line_code_space = 0
+        number_of_space = 0
+        i =0
+        # print("LEN 1",len(self.markdown_string))
+        # print("LEN 2",len(self.markdown_string.strip()))
+        text = self.markdown_string.strip()
         
-       
-        for line in self.markdown_string.splitlines():
+        text = text.replace("\r\n", "\n").replace("\r", "\n")
+        text += "\n\n"
+        text = text.expandtabs(4)
+        #print("TEXT",text)
+        # print("LEN 3",len(text))
+        # for i  in text.split("\n"):
+        #     print("_______________")
+        #     print("Line:",i)
+        #     for j in range(10,len(text.split("\n"))):
+        #         print(text.split("\n")[j])
+
+        #for line in self.markdown_string.splitlines():
+        for line  in text.split("\n"):
             print("_______________")
             print("Line:",line)
             
@@ -327,161 +343,201 @@ class markdown(object):
 
             #check if the line has single line code block
             if single_line_fenced_code.search(line) and multi_line_fenced_code_active ==False:
-                print("Single code line",line)
+                #print("Single code line",line)
                 single_line_fenced_code_active == True
                 codeline = self.single_line_code(single_line_fenced_code,line)
             else:
-                print("Multiple code line",line)
+                #print("Multiple code line",line)
                 multiple_line_fenced_code = re.compile(self.patterns["multi_line_fenced_code"])
       
                 if multi_line_fenced_code_active == True:
 
                     #keep appending code block line until the line matches another multi_line_fenced_code pattern
                     if multiple_line_fenced_code.search(line):
-                        print("SPACE multi code line")
-                        print("multi_line_code_space",multi_line_code_space)
-                        print("diff",len(line)-len(line.lstrip()))
+                        #print("SPACE multi code line")
+                        #print("multi_line_code_space",multi_line_code_space)
+                        #print("diff",len(line)-len(line.lstrip()))
                         # Close the multi line code block only if the line's space of first pattern match is equal to current line's space
                         if 0<=len(line)-len(line.lstrip())<=3:
                             multi_line_fenced_code_active = False
                             multi_line_code_space = 0
-                            print("code line close")
+                            #print("code line close")
                             codeline +="</code></pre>"
                         else:
                
                             codeline+='\n'+line+'\n'
-                            print("MULTILINE Code append 2",codeline)
+                            #print("MULTILINE Code append 2",codeline)
 
                             continue    
  
                     else:
                         #appends until pattern is match
                         codeline+='\n'+line+'\n'
-                        print("MULTILINE Code append 1",codeline)
+                        #print("MULTILINE Code append 1",codeline)
                         continue
                 else:
                     # check if the line is multi-line code block, 
                     # if so, set multi_line_fenced_code_active variable as True
                     #if "```" in line:
                     if multiple_line_fenced_code.search(line):
-                        print("MULTILINE Code start")
+                        #print("MULTILINE Code start")
                         multi_line_fenced_code_active = True
                         multi_line_code_space = len(line)-len(line.lstrip())
-                        print("multi_line_code_space",multi_line_code_space)
+                        #print("multi_line_code_space",multi_line_code_space)
                         if 0<=multi_line_code_space <=3:
 
                             codeline += "<pre><code>"
                         else:
                             codeline += "<pre><code>"+line
-                        print("Code line after start",codeline)
+                        #print("Code line after start",codeline)
                         continue
                     
             # It checks for other pattern only if its not a code block
             if multi_line_fenced_code_active ==False and len(codeline) == 0 and single_line_fenced_code_active ==False:
                 print("LINE:", line)
-                #heading    
-                heading = re.compile(self.patterns["heading"]) 
-                heading_matches = heading.search(line.strip())
-
-                if heading_matches != None:
-                    
-                    line = self.heading(heading_matches,line)
+                #heading   
                 
-                #bold and italic
-                bold_and_italic = re.compile(self.patterns["bold_and_italic"])
-                if bold_and_italic.search(line):
-                    line = self.bold_and_italic(bold_and_italic,line)
+                if line.strip() != "":  
+                    i+=1
+                    heading = re.compile(self.patterns["heading"]) 
+                    heading_matches = heading.search(line.strip())
+
+                    if heading_matches != None:
                         
+                        line = self.heading(heading_matches,line)
+                    
+                    #bold and italic
+                    bold_and_italic = re.compile(self.patterns["bold_and_italic"])
+                    if bold_and_italic.search(line):
+                        line = self.bold_and_italic(bold_and_italic,line)
                             
+                                
 
-                #HR tag
-                hr =  re.compile(self.patterns["hr"])
-                hr_match = hr.search(line)
-                if hr_match: 
-                    line = self.hr(hr,line)
+                    #HR tag
+                    hr =  re.compile(self.patterns["hr"])
+                    hr_match = hr.search(line)
+                    if hr_match: 
+                        line = self.hr(hr,line)
 
-                #Bold tag
-                bold = re.compile(self.patterns["bold"])
-                if bold.search(line):
-                    line = self.bold(bold,line)
+                    #Bold tag
+                    bold = re.compile(self.patterns["bold"])
+                    if bold.search(line):
+                        line = self.bold(bold,line)
 
-                #italic tag
-                italic = re.compile(self.patterns["italic"])
-        
-                if italic.search(line):
-        
-                    line = self.italic(italic,line)
-                
-
-                #Strikethrough
-                strikethrough = re.compile(self.patterns["strikethrough"])
+                    #italic tag
+                    italic = re.compile(self.patterns["italic"])
             
-                if strikethrough.search(line):
-                    line = self.strikethrough(strikethrough,line)
-
-                #ordered list
-                ordered_list = re.compile(self.patterns["ol"])
-                if ordered_list.search(line):
+                    if italic.search(line):
+            
+                        line = self.italic(italic,line)
                     
-                    line = self.ol_list(line,ordered_list)
 
-                else:
-                    if self.list_variable["ol"]["ol_tag_is_open"] == True:
-                        #if heading_matches or hr_match:
-                        line = self.close_list(line,"ol")
-                        self.ol_current_line_space = 0
-                        self.previous_ol_linespace =0
+                    #Strikethrough
+                    strikethrough = re.compile(self.patterns["strikethrough"])
+                
+                    if strikethrough.search(line):
+                        line = self.strikethrough(strikethrough,line)
 
-
-
-                #unordered list
-                unordered_list = re.compile(self.patterns["ul"],re.MULTILINE)
-
-                if unordered_list.search(line):
-                    line = self.list(line,unordered_list)
-                    
-                else:
-
-                    if self.list_variable["ul"]["ultag_is_open"] == True:
+                    #ordered list
+                    ordered_list = re.compile(self.patterns["ol"])
+                    #unordered list
+                    unordered_list = re.compile(self.patterns["ul"],re.MULTILINE)
+                    if ordered_list.search(line):
                         
-                        line = self.close_list(line,"ul") #close all opened list tags
-                        self.previous_ul_linespace = 0
+                        line = self.ol_list(line,ordered_list)
+
+                    else:
+                        if self.list_variable["ol"]["ol_tag_is_open"] == True:
+                            #if heading_matches or hr_match:
+                            for j in range(i, len(text.split("\n"))) :
+                                if text.split("\n")[j].strip() :
+                                    next =text.split("\n")[j]
+                                    break
+                            else:
+                                line = self.close_list(line,"ol")
+                                self.ol_current_line_space = 0
+                                self.previous_ol_linespace =0
+                            print("NEXT is ",next)
+                            print( "OL search",unordered_list.search(next), ordered_list.search(next))
+                            if not (ordered_list.search(next) or  unordered_list.search(next)): #dont close 
+                                line = self.close_list(line,"ol")
+                                self.ol_current_line_space = 0
+                                self.previous_ol_linespace =0
+           
+
+
+
+                    
+
+                    if unordered_list.search(line):
+                        line = self.list(line,unordered_list)
+                        
+                    else:
+                        if heading.search(line):
+                            print("HERE")
+                            line = self.close_list(line,"ul")
+
+                            self.previous_ul_linespace = 0
+                        else:
+                            if self.list_variable["ul"]["ultag_is_open"] == True:
+                                
+                                for j in range(i, len(text.split("\n"))):
+                                    
+                                    if text.split("\n")[j].strip() :
+                                        next = text.split("\n")[j]
+                                        break
+                                else:
+                                    line = self.close_list(line,"ul")
+                                    
+                                    self.previous_ul_linespace = 0
+                                print("NEXT is ",next)
+                                print( "UL next is",unordered_list.search(next), ordered_list.search(next),heading.search(next))
+
+                                if not (unordered_list.search(next) or ordered_list.search(next)) : #dont close 
+                                    line = self.close_list(line,"ul")
+
+                                    self.previous_ul_linespace = 0
+                                #line = self.close_list(line,"ul") #close all opened list tags
+                                #self.previous_ul_linespace = 0
+            
+
+                    #image
+                    #![Minion](https://octodex.github.com/images/minion.png)
+                    img = re.compile(self.patterns["image_links"])
+
+                    if img.search(line):
+                        line = self.image_links(img,line)
+
+                    else:    
+                        #links - example:[link text](https://www.google.com/)
+                        link_text =  re.compile(self.patterns["link_text"])
+
+                        if link_text.search(line):
+                            line = self.link_text(link_text,line)
+
+                        else:
         
+                            # Autoconverted link example:https://www.google.com/
+                            automatic_hyperlinks =  re.compile(self.patterns["automatic_hyperlinks"])
 
-                #image
-                #![Minion](https://octodex.github.com/images/minion.png)
-                img = re.compile(self.patterns["image_links"])
+                            if automatic_hyperlinks.search(line):
+                                line = self.automatic_hyperlinks(automatic_hyperlinks,line)
 
-                if img.search(line):
-                    line = self.image_links(img,line)
+                    #inline code
+                    inline_code = re.compile(self.patterns["inline_code"])
+                    if inline_code.search(line):
+                        line = self.inline(inline_code,line)  
 
-                else:    
-                    #links - example:[link text](https://www.google.com/)
-                    link_text =  re.compile(self.patterns["link_text"])
+                    #Making sure that an empty line is not added
+                    if line.strip() != "": #check if line is not empty
+                        pat =re.compile(r"(</*ul>|</*li>|</*ol>|</*h[1-6]{1}|</*code>|</*pre>|</*img>|</*a>|<hr>)")
+                        if pat.search(line):
+                            self.results += '\n'+ line +'\n'
+                        else:
+                            self.results+='\n'+"<p>"+line +"</p>"+'\n'
+                else:
+                    number_of_space+=1
 
-                    if link_text.search(line):
-                        line = self.link_text(link_text,line)
-
-                    else:
-    
-                        # Autoconverted link example:https://www.google.com/
-                        automatic_hyperlinks =  re.compile(self.patterns["automatic_hyperlinks"])
-
-                        if automatic_hyperlinks.search(line):
-                            line = self.automatic_hyperlinks(automatic_hyperlinks,line)
-
-                #inline code
-                inline_code = re.compile(self.patterns["inline_code"])
-                if inline_code.search(line):
-                    line = self.inline(inline_code,line)  
-
-                #Making sure that an empty line is not added
-                if line.strip() != "": #check if line is not empty
-                    pat =re.compile(r"(</*ul>|</*li>|</*ol>|</*h[1-6]{1}|</*code>|</*pre>|</*img>|</*a>|<hr>)")
-                    if pat.search(line):
-                        self.results += '\n'+ line +'\n'
-                    else:
-                        self.results+='\n'+"<p>"+line +"</p>"+'\n'
                 
             else:
                 #fenced code block
--- encyclopedia/templatetags/md.py
+++ encyclopedia/templatetags/md.py
@@ -1,7 +1,7 @@
 from django import template
 from django.template.defaultfilters import stringfilter
-from . import markdown as md
-
+#from . import markdown2 as md
+from . import markdown2html as md
 
 register = template.Library()
 
@@ -11,5 +11,6 @@ register = template.Library()
 def markdown(value):
 
     md_object = md.markdown(value)
+  
     return md_object.markdown_parser()
 
